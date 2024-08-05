5e7cb627b53e45fe1de61c42d7fd1b72f6360592
Mozgarik <91159717+Mozgarik@users.noreply.github.com>
2024-08-05 17:54:19 +0000

    Deploying to gh-pages from @ Mozgarik/outlast-quest@105d450b6933d8f1d4422eaf44af0a04d1b038fb 🚀

--- src/Components/Logo/Logo.jsx
+++
@@ -1,8 +0,0 @@
-import logo from '../../img/logo.svg'
-import s from './logo.module.css'
-
-export default function Logo() {
-    return(
-        <img src={logo} className={s.logo} alt=""/>
-    )
-}
--- src/Components/questItem/QuestItem.jsx
+++
@@ -1,7 +0,0 @@
-
-export default function QuestItem({ img, name, tittle }) {
-    return(
-        <li><img src={img} width={'100px'} alt="" /><h2>{name}</h2><h3>{tittle}</h3></li>
-    )
-
-}
